00bb4b23389512ea6bcddbf00728c93ca04470df
Marco Maggi <marco.maggi-ipsu@poste.it>
2012-08-14 08:18:07 +0200

    modified infrastructure to enable debugging features with configure option and to put temporary fasl files under builddir

--- configure.sh
+++ configure.sh
@@ -10,6 +10,7 @@ prefix=/usr/local
     --config-cache                              \
     --cache-file=../config.cache                \
     --prefix="${prefix}"                        \
+    --enable-debug                              \
     CFLAGS='-Wall -O3 -march=i686 -mtune=i686'  \
     "$@"
 
