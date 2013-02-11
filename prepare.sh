2ddd337c3992edb06aa9063d4ebe16e0259a517f
Marco Maggi <marco.maggi-ipsu@poste.it>
2013-02-11 08:10:19 +0100

    tweaking infrastructure file

--- prepare.sh
+++ prepare.sh
@@ -4,16 +4,8 @@
 
 set -xe
 
-(set -xe ;  cd .. && sh autogen.sh)
-
-prefix=/usr/local
-
-../configure \
-    --config-cache                              \
-    --cache-file=../config.cache                \
-    --disable-static --enable-shared            \
-    --prefix="${prefix}"                        \
-    CFLAGS='-Wall -O3 -march=i686 -mtune=i686'  \
-    "$@"
+(cd .. && sh autogen.sh)
+sh ../configure.sh
 
 ### end of file
+
